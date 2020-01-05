ee5681ddc29fce02b0c0c9afe657ea99ecba2191
ndo@ndo3 <yo@ni.co.de>
2020-01-05 22:50:22 +0100

    test: web share target API

--- src/components/serviceWorker.js
+++ src/components/serviceWorker.js
@@ -10,17 +10,15 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-// const isLocalhost = Boolean(
-//   window.location.hostname === 'localhost' ||
-//     // [::1] is the IPv6 localhost address.
-//     window.location.hostname === '[::1]' ||
-//     // 127.0.0.0/8 are considered localhost for IPv4.
-//     window.location.hostname.match(
-//       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-//     )
-// )
-
-const isLocalhost = false
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+)
 
 export function register (config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
