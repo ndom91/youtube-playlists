532d2a8198fc4cbcdef1c82fe5328892935b0810
ndom91 <yo@ndo.dev>
2022-06-08 19:59:02 +0200

    fix: remove styled-components from modal.jsx

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import * as S from './styled'
 
 const Modal = (props) => {
   const { handleClose, handleAdd, show, children } = props
 
-  const showHideClassName = show ? 'display-block' : 'display-none'
-
   return (
-    <S.Modal className={showHideClassName}>
-      <section>
-        {children}
-        <S.ModalBtn className="btn-add" onClick={handleAdd}>
+    <div
+      className={`${
+        show ? 'block' : 'hidden'
+      } z-[200] transition rounded-lg border-8 border-white absolute top-1/2 left-1/2 translate-y-1/2 -translate-x-1/2   bg-[linear-gradient(to_top,_#d299c2_0%,_#fef9d7_100%)] shadow-md`}
+    >
+      {children}
+      <section className="absolute bottom-4 right-20 flex space-x-8">
+        <button
+          className="px-3 py-2 text-white font-semibold transition hover:bg-purple-600 bg-purple-400 shadow-md rounded-xl  ring-2 ring-purple-500 hover:ring-purple-800 hover:shadow-lg"
+          onClick={handleAdd}
+        >
           Add
-        </S.ModalBtn>
-        <S.ModalBtn className="btn-close" onClick={handleClose}>
+        </button>
+        <button
+          className="px-3 py-2 text-white font-semibold  hover:bg-purple-600 transition bg-purple-400 shadow-md rounded-xl ring-2 ring-purple-500  hover:ring-purple-800 hover:shadow-lg"
+          onClick={handleClose}
+        >
           Close
-        </S.ModalBtn>
+        </button>
       </section>
-    </S.Modal>
+    </div>
   )
 }
 
