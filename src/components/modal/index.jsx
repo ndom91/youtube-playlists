849d64e5796bea3bc2aefc1115df8e1b9ebec780
ndom91 <yo@ndo.dev>
2022-06-08 20:01:07 +0200

    feat: modal backdrop

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -4,26 +4,29 @@ const Modal = (props) => {
   const { handleClose, handleAdd, show, children } = props
 
   return (
-    <div
-      className={`${
-        show ? 'block' : 'hidden'
-      } z-[200] transition rounded-lg border-8 border-white absolute top-1/2 left-1/2 translate-y-1/2 -translate-x-1/2   bg-[linear-gradient(to_top,_#d299c2_0%,_#fef9d7_100%)] shadow-md`}
-    >
-      {children}
-      <section className="absolute bottom-4 right-20 flex space-x-8">
-        <button
-          className="px-3 py-2 text-white font-semibold transition hover:bg-purple-600 bg-purple-400 shadow-md rounded-xl  ring-2 ring-purple-500 hover:ring-purple-800 hover:shadow-lg"
-          onClick={handleAdd}
-        >
-          Add
-        </button>
-        <button
-          className="px-3 py-2 text-white font-semibold  hover:bg-purple-600 transition bg-purple-400 shadow-md rounded-xl ring-2 ring-purple-500  hover:ring-purple-800 hover:shadow-lg"
-          onClick={handleClose}
-        >
-          Close
-        </button>
-      </section>
+    <div className="z-[200]">
+      <div
+        className={`${
+          show ? 'block' : 'hidden'
+        } z-[200] transition rounded-lg border-8 border-white absolute top-1/2 left-1/2 translate-y-1/2 -translate-x-1/2   bg-[linear-gradient(to_top,_#d299c2_0%,_#fef9d7_100%)] shadow-md`}
+      >
+        {children}
+        <section className="absolute bottom-4 right-20 flex space-x-8">
+          <button
+            className="px-3 py-2 text-white font-semibold transition hover:bg-purple-600 bg-purple-400 shadow-md rounded-xl  ring-2 ring-purple-500 hover:ring-purple-800 hover:shadow-lg"
+            onClick={handleAdd}
+          >
+            Add
+          </button>
+          <button
+            className="px-3 py-2 text-white font-semibold  hover:bg-purple-600 transition bg-purple-400 shadow-md rounded-xl ring-2 ring-purple-500  hover:ring-purple-800 hover:shadow-lg"
+            onClick={handleClose}
+          >
+            Close
+          </button>
+        </section>
+      </div>
+      <div className="fixed w-full h-full top-0 left-0 bg-black opacity-60" />
     </div>
   )
 }
