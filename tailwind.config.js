a5f0b778bee233b7c052e1c4219410861a86fccc
ndom91 <yo@ndo.dev>
2022-06-08 19:29:38 +0200

    feat: update dependencies + add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    'src/components/**/*.{js,jsx}',
+    'src/lib/**/*.{js,jsx}',
+    'src/pages/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
